af70eaa9607830b4322e63e44b1a17c85b46df70
Nathan George <georgenathan010@gmail.com>
2024-02-05 16:10:19 +0300

    made a basic server

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
-console.log('Task Manager App')
+const express = require('express')
+
+const app = express()
+const PORT = 3000 || process.env.PORT
+
+app.use('/', (req,res)=> {
+    res.send('hello')
+})
+
+
+
+app.listen(PORT, ()=> {
+    console.log(`listening on ${PORT}`)
+})
