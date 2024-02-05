b0a4ac8682f74b9072ddb589ab34cca7578436b1
Nathan George <georgenathan010@gmail.com>
2024-02-05 16:30:07 +0300

    added the static server

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express')
 
 const app = express()
+const path = require('path')
 const PORT = 3000 || process.env.PORT
 
+//static files
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', (req,res)=> {
     res.send('hello')
 })
