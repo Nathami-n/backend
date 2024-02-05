9abe8c8c1d89e5d04414c179bade0db5593d4aa8
Nathan George <georgenathan010@gmail.com>
2024-02-05 20:50:17 +0300

    stripped out everything

--- 02-express-tutorial/logger.js
+++
@@ -1,8 +0,0 @@
-const logger = (req,res,next) => {
-    const method = req.method;
-    const url = req.url;
-    const time = new Date().getFullYear();
-    next();
-  };
-
-  module.exports = {logger}
